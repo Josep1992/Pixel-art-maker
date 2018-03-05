8d01daaf2a3d8c72ee64d3d60943005cea30e6f2
Josep <coding1992@outlook.com>
2018-03-05 17:28:55 -0400

    Initial commit

---
+++ designs.js
@@ -0,0 +1,49 @@
+let height, weight, grid, form, color, tableRow, tableCell, resetBtn, clearBtn;
+
+height = $('#inputHeight');
+width = $('#inputWeight');
+color = $('#colorPicker');
+
+canvas = $('#pixelCanvas');
+form = $('#sizePicker');
+
+resetBtn = $('#reset');
+clearBtn = $('#clear');
+
+//Starts the page with a grid of 5 table rows and 5 table cells
+const makeGrid = () => {
+  for (row = 0; row < height.val(); row++) {
+    tableRow = $('<tr></tr>');
+    for (column = 0; column < width.val(); column++) {
+      tableCell = $('<td></td>');
+      tableRow.append(tableCell);
+    }
+    canvas.append(tableRow);
+  }
+};
+
+// On submit form creates a grid with the given values on each input
+form.on('submit', e => {
+  makeGrid();
+  e.preventDefault();
+});
+
+// On click table cell background becomes that of the color input
+canvas.on('click', 'td', function() {
+  $(this).css('background-color', `${color.val()}`);
+});
+
+// On double click table cell background becomes transparent
+canvas.on('dblclick', 'td', function() {
+  $(this).css('background-color', 'transparent');
+});
+
+// On mouse down on the reset button reload page.
+resetBtn.on('mousedown', () => {
+  window.location.reload();
+});
+
+// on mouse down clears the colors of the grid
+clearBtn.on('mousedown', () => {
+  $('td').css('background-color', 'transparent');
+});
